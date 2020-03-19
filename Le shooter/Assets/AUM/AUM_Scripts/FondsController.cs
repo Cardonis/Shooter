0cc3e4cd39d30e2457230d6fb942493ec56d7bfa
Aurélien <orelien.mary@gmail.com>
2020-03-19 15:38:05 +0100

    BackGround Progressing Done

--- Le shooter/Assets/AUM/AUM_Scripts/FondsController.cs
+++ Le shooter/Assets/AUM/AUM_Scripts/FondsController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class FondsController : MonoBehaviour
 {
+    public Transform fonds;
+
     public GameObject player;
 
     public GameObject fondPrefab;
@@ -15,17 +17,37 @@ public class FondsController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        firstFond = fondPrefab;
-        secondFond = fondPrefab;
-        thirdFond = fondPrefab;
+        firstFond = Instantiate(fondPrefab, fonds);
+        secondFond = Instantiate(fondPrefab, fonds);
+        thirdFond = Instantiate(fondPrefab, fonds);
+
+        firstFond.transform.position = new Vector2(secondFond.transform.position.x, transform.position.y);
+
+        secondFond.transform.position = new Vector2(secondFond.transform.position.x, firstFond.transform.position.y + 9.95f);
+
+        thirdFond.transform.position = new Vector2(secondFond.transform.position.x, secondFond.transform.position.y + 9.95f);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(secondFond.transform.position.y + 3 < player.transform.position.y)
+        if(secondFond.transform.position.y + 6 < player.transform.position.y)
         {
-
+            Cycle();
         }
+    }
+
+    void Cycle()
+    {
+        Destroy(firstFond);
+
+        firstFond = secondFond;
+        secondFond = thirdFond;
+
+        thirdFond = Instantiate(fondPrefab, fonds);
+
+        thirdFond.transform.position = new Vector2(secondFond.transform.position.x, secondFond.transform.position.y + 9.95f);
+
     }
 }

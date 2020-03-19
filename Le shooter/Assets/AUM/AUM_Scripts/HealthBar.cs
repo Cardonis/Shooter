c96a4194a66fcf61dc85f2374c89619f4afbcb2f
Aurélien <orelien.mary@gmail.com>
2020-03-19 16:21:17 +0100

    Life showed

---
+++ Le shooter/Assets/AUM/AUM_Scripts/HealthBar.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    public SpriteRenderer[] lifes;
+
+    public Player_Movement player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0; i < lifes.Length; i++)
+        {
+            if(player.life > i)
+            {
+                lifes[i].enabled = true;
+            }
+            else
+            {
+                lifes[i].enabled = false;
+            }
+        }
+
+    }
+}

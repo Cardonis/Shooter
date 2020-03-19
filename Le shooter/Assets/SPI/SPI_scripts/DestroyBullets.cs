3f50546f0f8392f13872f59629487f2b413a79ce
Cardo <S.picardat@rubika-edu.com>
2020-03-19 11:05:44 +0100

    Auto stash before merge of "SPI_19_11h" and "origin/MGH_11h"

---
+++ Le shooter/Assets/SPI/SPI_scripts/DestroyBullets.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBullets : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("EnnemiBullet") || collision.CompareTag("PlayerBullet"))
+        {
+            Destroy(collision);
+        }
+    }
+}
